cde2eb892aca27b9cebb1f99c7ed3e679816f619
J. Kyle Berry <31809859+berrytechnics@users.noreply.github.com>
2022-12-20 23:47:46 -0600

    done for the night

--- src/main.js
+++ src/main.js
@@ -2,15 +2,6 @@ import express from 'express'
 import './database.js'
 const app = express()
 app.get('/',(req,res,next)=>res.sendStatus(200))
-app.use((err,req,res,next)=>{
-    if(!err) next()
-    console.error(err.stack)
-    res.send(err.stack)
-})
-app.use((req,res)=>{
-    const err = new Error(`An unknown error occurred!`)
-    console.error(err)
-    res.status(500).send(err.stack)
-})
+app.use((err,req,res,next)=>err?res.send(err.message):res.sendStatus(404))
 app.listen(process.env.PORT,()=>console.log(`Listening on http://localhost:80  -:${process.env.PORT}`))
 
